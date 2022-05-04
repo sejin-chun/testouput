ffbf9354b982132165a2fb0af5ea6085d6736c24
sejin-chun <101098015+sejin-chun@users.noreply.github.com>
2022-05-04 10:49:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+   include_package_data = True ,
+   name = 'testoutput12345',
+   version = '1.0.0',
+   description = 'oss- development class test output',
+   author = 'sejinchun',
+   author_email = 'sjchun@donga.ac.kr',
+   url = 'https://github.com/sejin-chun/testouput/',
+   install_requires = ['pytest'],
+   long_description = 'oss development ...',
+   long_description_content_type = 'text/markdown',
+   classifiers = [                 
+     "Programming Language :: Python :: 3",
+     "Operating System :: OS,
+   ],
+)
